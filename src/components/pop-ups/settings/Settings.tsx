71f7bc1c18a3fe538e4fd86af76b59f35c3be6ad
Ace Baum <jaybaum.03@gmail.com>
2025-04-23 20:33:31 -0400

    settings popup refactor

--- src/components/pop-ups/settings/Settings.tsx
+++ src/components/pop-ups/settings/Settings.tsx
@@ -24,148 +24,71 @@ function Settings({
   setSelectedEra,
 }: SettingsProps) {
   const [dropdownActive, setDropdownActive] = useState({ active: false });
-  const eraYears = [
-    "All Years",
-    "2010-Pres.",
-    "2000-2009",
-    "1990-1999",
-    "1980-1989",
-    "1968-1979",
-  ];
 
-  const handleExitClick = () => {
+  enum ExitClickSource {
+    NewPlayer,
+    ResetBoard,
+    Other,
+  }
+
+  function handleExitingClick(source?: ExitClickSource) {
+    if (source === ExitClickSource.NewPlayer) {
+      selectNewPlayer(filterData());
+    } else if (source === ExitClickSource.ResetBoard) {
+      resetGame();
+    }
+
     setDropdownActive({ active: false });
     setPopupActive({ settings: false });
-  };
+  }
 
-  const handleDropdownClick = () => {
-    if (dropdownActive.active) {
-      setDropdownActive({ active: false });
-    } else {
-      setDropdownActive({ active: true });
-    }
-  };
+  const EraDropdown = () => {
+    const handleDropdownClick = () => {
+      setDropdownActive({ active: !dropdownActive.active });
+    };
 
-  const handleNewPlayerClick = () => {
-    // Select a new player from the filtered data
-    selectNewPlayer(filterData());
-
-    // Quit from the settings menu
-    handleExitClick();
-  };
-
-  const handleResetClick = () => {
-    // Reset the game
-    resetGame();
-
-    // Quit from the settings menu
-    handleExitClick();
-  };
-
-  const handleDropdownItemClick = (id: Era) => {
-    // Is the id equal to the already selected era?
-    if (id !== selectedEra) {
-      setSelectedEra(id);
+    function handleDropdownItemClick(era: Era) {
+      setSelectedEra(era);
       handleDropdownClick();
+    };
+
+    const EraDropdownItem = ({ era }: { era: Era }) => {
+      return (
+        <div
+          className="eraDropdownItem"
+          onClick={() => {
+            handleDropdownItemClick(era);
+          }}
+        >
+          <p>{era}</p>
+        </div>
+      )
     }
-  };
 
-  return (
-    <div id={popupActive.settings ? "show" : "hide"}>
-      <div className="popupWall" />
-      <div className="popup" id="settingsPopup">
-        <button
-          className="exitButton"
-          id="settingsExit"
-          onClick={() => {
-            handleExitClick();
-          }}
-        >
-          X
-        </button>
+    return (
+      <>
         <div
-          className="newPlayerButton"
+          className="eraDropdownButton"
           onClick={() => {
-            handleNewPlayerClick();
+            handleDropdownClick();
           }}
+          id={dropdownActive.active ? "active" : "inactive"}
         >
-          <p>NEW PLAYER</p>
+          <p>{selectedEra}</p>
         </div>
         <div
-          className="resetBoardButton"
-          onClick={() => {
-            handleResetClick();
-          }}
+          className="eraDropdownContent"
+          id={dropdownActive.active ? "show" : "hide"}
         >
-          <p>RESET BOARD</p>
+          <EraDropdownItem era={Era.All} />
+          <EraDropdownItem era={Era.TwoThousandTens} />
+          <EraDropdownItem era={Era.TwoThousands} />
+          <EraDropdownItem era={Era.Nineties} />
+          <EraDropdownItem era={Era.Eighties} />
+          <EraDropdownItem era={Era.Seventies} />
         </div>
-        <div className="eraDropdown">
-          <p className="eraDropdownLabel">Selected Years:</p>
-          <div
-            className="eraDropdownButton"
-            onClick={() => {
-              handleDropdownClick();
-            }}
-            id={dropdownActive.active ? "active" : "inactive"}
-          >
-            <p>{eraYears[selectedEra]}</p>
-          </div>
-
-          <div
-            className="eraDropdownContent"
-            id={dropdownActive.active ? "show" : "hide"}
-          >
-            <div
-              className="eraDropdownItem"
-              onClick={() => {
-                handleDropdownItemClick(Era.AllPlayers);
-              }}
-            >
-              <p>{eraYears[0]}</p>
-            </div>
-            <div
-              className="eraDropdownItem"
-              onClick={() => {
-                handleDropdownItemClick(Era.TwoThousandTens);
-              }}
-            >
-              <p>{eraYears[1]}</p>
-            </div>
-            <div
-              className="eraDropdownItem"
-              onClick={() => {
-                handleDropdownItemClick(Era.TwoThousands);
-              }}
-            >
-              <p>{eraYears[2]}</p>
-            </div>
-            <div
-              className="eraDropdownItem"
-              onClick={() => {
-                handleDropdownItemClick(Era.Nineties);
-              }}
-            >
-              <p>{eraYears[3]}</p>
-            </div>
-            <div
-              className="eraDropdownItem"
-              onClick={() => {
-                handleDropdownItemClick(Era.Eighties);
-              }}
-            >
-              <p>{eraYears[4]}</p>
-            </div>
-            <div
-              className="eraDropdownItem"
-              onClick={() => {
-                handleDropdownItemClick(Era.Seventies);
-              }}
-              id="lastItem"
-            >
-              <p>{eraYears[5]}</p>
-            </div>
-          </div>
-          {dropdownActive.active ? (
+        {
+          dropdownActive.active ? (
             <KeyboardArrowUpIcon
               className="dropdownArrowIcon"
               onClick={() => {
@@ -179,10 +102,41 @@ function Settings({
                 handleDropdownClick();
               }}
             />
-          )}
+          )
+        }
+      </>
+    )
+  }
+
+  return (
+    <div id={popupActive.settings ? "show" : "hide"}>
+      <div className="popupWall" />
+      <div className="popup" id="settingsPopup">
+        <button
+          className="exitButton"
+          id="settingsExit"
+          onClick={() => handleExitingClick(ExitClickSource.Other)}
+        >
+          X
+        </button>
+        <div
+          className="newPlayerButton"
+          onClick={() => handleExitingClick(ExitClickSource.NewPlayer)}
+        >
+          <p>NEW PLAYER</p>
+        </div>
+        <div
+          className="resetBoardButton"
+          onClick={() => handleExitingClick(ExitClickSource.ResetBoard)}
+        >
+          <p>RESET BOARD</p>
+        </div>
+        <div className="eraDropdown">
+          <p className="eraDropdownLabel">Selected Years:</p>
+          <EraDropdown />
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
