c878beef6389dffa06044ce2100cf10ff95b3a7a
Ace Baum <jaybaum.03@gmail.com>
2025-07-12 19:06:23 -0400

    update pages injections

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
     /* config options here */
     basePath: '/bengle',
     output: 'export',
+    images: {
+        unoptimized: true,
+    },
 }
 
 export default nextConfig
