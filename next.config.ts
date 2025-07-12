1e52e8ad01fec6a457d248ff8cf14a72eada241d
Ace Baum <jaybaum.03@gmail.com>
2025-07-12 19:35:29 -0400

    feature/deployment (#12)
    * add github actions
    
    * add debug statement
    
    * update node version
    
    * update pages injections
    
    * attempt to fix next.config file
    
    * add nojekyll
    
    * try new publish
    
    * run on all branches
    
    * rename
    
    * fix all branches bug
    
    * try pnpm fix
    
    * remove pnpm cache

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    /* config options here */
+    basePath: '/bengle',
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+}
 
-export default nextConfig;
+export default nextConfig
