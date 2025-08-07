074ffe39f7a3ca76c3ba9998ca0d43d1515f04eb
Ace Baum <jaybaum.03@gmail.com>
2025-08-06 20:43:34 -0400

    remove static build

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
     /* config options here */
     basePath: '/bengle',
-    output: 'export',
     images: {
         unoptimized: true,
     },
